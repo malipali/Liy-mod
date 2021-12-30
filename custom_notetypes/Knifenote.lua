43200177ca5c92ed9a03543bd6643c2f0a8a8b6f
Igorogamer <dasilvacezarigor@gmail.com>
2021-12-30 18:37:27 -0300

    Added knife notes

---
+++ custom_notetypes/Knifenote.lua
@@ -0,0 +1,33 @@
+function onCreate()
+	--Iterate over all notes
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		--Check if the note is an Knife note
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Knifenote' then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'Knifenote'); --Change texture
+
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
+				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has penalties
+			end
+		end
+	end
+	--debugPrint('Script started!')
+end
+
+-- Function called when you hit a note (after note hit calculations)
+-- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
+-- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
+-- noteType: The note type string/tag
+-- isSustainNote: If it's a hold note, can be either true or false
+function goodNoteHit(id, noteData, noteType, isSustainNote)
+	if noteType == 'Knifenote' then
+		-- put something here if you want
+	end
+end
+
+-- Called after the note miss calculations
+-- Player missed a note by letting it go offscreen
+function noteMiss(id, noteData, noteType, isSustainNote)
+	if noteType == 'Knifenote' then
+		setProperty('health', -500)
+	end
+end
